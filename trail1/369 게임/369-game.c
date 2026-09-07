77dd272e418a14f7b9936f843e74f87c0db02db2
양우찬 <133564503+staypleur@users.noreply.github.com>
2026-09-07 12:45:38 +0900

    [Passed] 369 게임 (1ms, 0MB)

--- trail1/369 게임/369-game.c
+++ trail1/369 게임/369-game.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 
-int main(void) {
-    int N, i, n, check;
+int main() {
+    // 변수 선언
+    int n;
 
-    scanf("%d", &N);
-
-    for (i = 1; i <= N; i++) {
-        check = 0;
-
-        if (i % 3 == 0)
-            check = 1;
-
-        for (n = i; n > 0; n /= 10) {
-            if (n % 10 == 3 || n % 10 == 6 || n % 10 == 9)
-                check = 1;
-        }
-
-        if (check)
+    // 입력
+    scanf("%d", &n);
+    
+    // 출력
+    for(int i = 1; i <= n; i++) {
+        if(i % 3 == 0)
+            printf("0 ");
+        else if(i % 10 == 3 || i % 10 == 6 || i % 10 == 9)
+            printf("0 ");
+        else if(i / 10 == 3 || i / 10 == 6 || i / 10 == 9)
             printf("0 ");
         else
             printf("%d ", i);
     }
-
+  
     return 0;
 }
+
