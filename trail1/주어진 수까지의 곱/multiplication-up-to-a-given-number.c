0f2922c32180078e6cf5486e137481e927859bec
양우찬 <133564503+staypleur@users.noreply.github.com>
2026-09-07 20:11:44 +0900

    [Passed] 주어진 수까지의 곱 (1ms, 0MB)

---
+++ trail1/주어진 수까지의 곱/multiplication-up-to-a-given-number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    int sum = 1;
+    scanf("%d %d", &a, &b);
+    for (int i = a; i <= b; i++) {
+        sum *= i;
+    }
+    printf("%d", sum);
+    // Please write your code here.
+    return 0;
+}
