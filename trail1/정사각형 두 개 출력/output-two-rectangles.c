0db6e6e6f882a467890c34d891a6540ab551d111
양우찬 <133564503+staypleur@users.noreply.github.com>
2026-09-09 11:21:12 +0900

    [Passed] 정사각형 두 개 출력 (1ms, 0MB)

---
+++ trail1/정사각형 두 개 출력/output-two-rectangles.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                printf("*");
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+    // Please write your code here.
+    return 0;
+}
