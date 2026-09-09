e165b1e3ec53ea208f256a3dc49691eecf717cdc
양우찬 <133564503+staypleur@users.noreply.github.com>
2026-09-09 13:15:48 +0900

    [Passed] 건너뛰고 별표 출력하기 (1ms, 0MB)

---
+++ trail1/건너뛰고 별표 출력하기/skip-and-print-a-star.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+        printf("\n");
+    }
+    for (int i = n-1; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            printf("*");
+        }
+        printf("\n");
+        printf("\n");
+    }
+    // Please write your code here.
+    return 0;
+}
