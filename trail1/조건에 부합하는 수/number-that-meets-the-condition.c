2476007ba18553fa2cd5e2923a10b64b9d15ecde
양우찬 <133564503+staypleur@users.noreply.github.com>
2026-09-09 09:08:13 +0900

    [Passed] 조건에 부합하는 수 (1ms, 0MB)

---
+++ trail1/조건에 부합하는 수/number-that-meets-the-condition.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    for (int i = 1; i <= a; i++) {
+        if (i % 2 == 0 && i % 4 != 0) {
+            continue;
+        }
+        if ((i / 8) % 2 == 0) {
+            continue;
+        }
+        if ((i % 7) < 4) {
+            continue;
+        }
+        printf("%d ", i);
+    }
+    // Please write your code here.
+    return 0;
+}
