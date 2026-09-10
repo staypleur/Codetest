fcf0a1eea2ed4f2219fc526f458c51ab7c7f4315
양우찬 <133564503+staypleur@users.noreply.github.com>
2026-09-10 10:17:35 +0900

    [Passed] 두 개의 직각삼각형 (1ms, 0MB)

---
+++ trail1/두 개의 직각삼각형/two-right-triangle.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i = n; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            printf("*");
+        }
+
+        for (int j = 0; j < 2 * (n - i); j++) {
+            printf(" ");
+        }
+        for (int j = 0; j < i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    // Please write your code here.
+    return 0;
+}
