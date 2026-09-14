17f74bb610f3dc715560d29e6300091825704198
양우찬 <133564503+staypleur@users.noreply.github.com>
2026-09-14 10:30:02 +0900

    [Passed] 별표 출력하기 10 (1ms, 0MB)

---
+++ trail1/별표 출력하기 10/print-star-10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < 2 * n; i++) {
+        if(i % 2 == 0) {
+            for (int j = 0; j < 1 + (i / 2); j++) {
+                printf("* ");
+            }
+            printf("\n");
+        }
+        else {
+            for (int j = 0; j < n - (i - 1) / 2; j++) {
+                printf("* ");
+            }
+            printf("\n");
+        }
+    } 
+    // Please write your code here.
+    return 0;
+}
