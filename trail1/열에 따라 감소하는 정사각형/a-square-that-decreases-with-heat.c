4e6da1eeedc735079fd38c7c18ed511b26369163
양우찬 <133564503+staypleur@users.noreply.github.com>
2026-09-14 13:13:11 +0900

    [Passed] 열에 따라 감소하는 정사각형 (1ms, 0MB)

---
+++ trail1/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", n - j);
+        }
+        printf("\n");
+    }
+    // Please write your code here.
+    return 0;
+}
