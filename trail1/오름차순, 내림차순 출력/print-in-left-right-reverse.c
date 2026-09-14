ef3cea6fc418cb205141ffa176310106574077d4
양우찬 <133564503+staypleur@users.noreply.github.com>
2026-09-14 14:12:09 +0900

    [Passed] 오름차순, 내림차순 출력 (1ms, 0MB)

---
+++ trail1/오름차순, 내림차순 출력/print-in-left-right-reverse.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (i % 2 == 0) {
+                printf("%d", j);
+            } else {
+                printf("%d", n - j + 1);
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
