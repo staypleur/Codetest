dabd0fbf377c725ea70c875595b7adbb0307d730
양우찬 <133564503+staypleur@users.noreply.github.com>
2026-09-21 14:57:44 +0900

    [Passed] N개의 정수 중 최대 2개 (1ms, 0MB)

---
+++ trail1/N개의 정수 중 최대 2개/two-max-of-n-num.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] < arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    printf("%d %d", arr[0], arr[1]);
+    // Please write your code here.
+    return 0;
+}
