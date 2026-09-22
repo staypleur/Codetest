2f67091d64322e6a2d822a412ce3c30c2cb7c21c
양우찬 <133564503+staypleur@users.noreply.github.com>
2026-09-22 11:47:41 +0900

    [Passed] 두 배열의 곱 (2ms, 0MB)

---
+++ trail1/두 배열의 곱/multiple-of-two-arrays.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int arr1[3][3];
+    int arr2[3][3];
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &arr1[i][j]);    
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &arr2[i][j]);    
+        }
+        
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", arr1[i][j] * arr2[i][j]);
+        }
+        printf("\n");
+    }
+    // Please write your code here.
+    return 0;
+}
