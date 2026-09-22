67bb16c5fed3dc13ab95149736909b00f88e29a1
양우찬 <133564503+staypleur@users.noreply.github.com>
2026-09-22 16:25:35 +0900

    [Passed] 격자 반대로 채우기 (1ms, 0MB)

--- trail1/격자 반대로 채우기/grid-anti-fill.c
+++ trail1/격자 반대로 채우기/grid-anti-fill.c
@@ -7,44 +7,20 @@ int main() {
     int arr[n][n];
     int num = 1;
     
-    if (n % 2 == 0) {
-        for (int j = n-1; j >= 0; j--) {
+    for (int j = n - 1; j >= 0; j--) {
+        if ((n - 1 - j) % 2 == 0) {
+            for (int i = n - 1; i >= 0; i--) {
+                arr[i][j] = num;
+                num++;
+            }
+        }
 
-            if (j % 2 == 1) {
-                for (int i = n-1; i >= 0; i--) {
-                    arr[i][j] = num;
-                    num++;
-                }
+        else {
+            for (int i = 0; i < n; i++) {
+                arr[i][j] = num;
+                num++;
             }
-        
-            else {
-                for (int i = 0; i < n; i++) {
-                    arr[i][j] = num;
-                    num++;
-                }
-            }
-        
-        } 
-    }
-
-    else {
-        for (int j = n-1; j >= 0; j--) {
-
-            if (j % 2 == 0) {
-                for (int i = n-1; i >= 0; i--) {
-                    arr[i][j] = num;
-                    num++;
-                }
-            }
-        
-            else {
-                for (int i = 0; i < n; i++) {
-                    arr[i][j] = num;
-                    num++;
-                }
-            }
-        
-        } 
+        }
     }
 
     for (int i = 0; i < n; i++) {
