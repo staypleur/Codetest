3d5101686eea48c2536f95022ca364585a6d9d17
양우찬 <133564503+staypleur@users.noreply.github.com>
2026-09-22 16:37:59 +0900

    [Passed] 파스칼의 삼각형 (1ms, 0MB)

---
+++ trail1/파스칼의 삼각형/pascals-triangle.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n][n];
+
+    for (int i = 0; i < n; i++) {
+        arr[i][0] = 1;
+        for (int j = 0; j < n; j++) {
+            if (i == j) {
+                arr[i][j] = 1;
+            }
+            
+        }
+    }
+
+    for (int i = 2; i < n; i++) {
+        for (int j = 1; j < i; j++) {
+            arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+            
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    
+    // Please write your code here.
+    return 0;
+}
